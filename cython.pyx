55e3582984b68bc0ac43a553af70a78931cc850a
GE <geziyu1@126.com>
2016-06-17 10:34:23 +0800

    Add Member Function

--- cython.pyx
+++ cython.pyx
@@ -27,3 +27,38 @@ def int(int h, int w, int d):
     height = h
     width = w
     disp_max = d
+
+def ad_vol(np.ndarray[np.float64_t, ndim=3] x0, np.ndarray[np.float64_t, ndim=3] x1):
+    cdef np.ndarray[np.float64_t, ndim=3] res
+    cdef int d, i, j, c
+
+    res = np.zeros((disp_max, height, width))
+    for d in range(disp_max):
+        for i in range(height):
+            for j in range(width):
+                if j - d < 0:
+                    res[d,i,j] = INFINITY
+                else:
+                    for c in range(3):
+                        res[d,i,j] += abs(x0[i,j,c] - x1[i,j-d,c])
+                    res[d,i,j] /= 3
+    return res
+    
+def census_transform(np.ndarray[np.float64_t, ndim=3] x):
+    cdef np.ndarray[np.int_t, ndim=3] cen
+    cdef int i, j, ii, jj, k, ind, ne
+
+    ne = np.random.randint(2**31-1)
+    cen = np.zeros((height, width, 63 * 3), dtype=np.int)
+    for i in range(height):
+        for j in range(width):
+            ind = 0
+            for ii in range(i - 3, i + 4):
+                for jj in range(j - 4, j + 5):
+                    for k in range(3):
+                        if 0 <= ii < height and 0 <= jj < width:
+                            cen[i, j, ind] = x[ii, jj, k] < x[i, j, k]
+                        else:
+                            cen[i, j, ind] = ne
+                        ind += 1
+    return cen
